9a97e10472f155cb3ad6157c77ad1f5dd86b1f1d
Kevin S. <kefin87@gmail.com>
2024-05-26 18:23:02 +0200

    add folder structure

---
+++ Day1/main.py
@@ -0,0 +1,8 @@
+print("What is your hometown?")
+first_part = input()
+
+print("What is your pet's name?")
+second_part = input()
+
+name = first_part + " " + second_part
+print(f"Your band name is: {name}.")
