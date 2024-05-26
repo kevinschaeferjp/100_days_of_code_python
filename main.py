1d30b5bc70973fe0e7c86d11106551824e34ef22
Kevin S. <kefin87@gmail.com>
2024-05-26 18:22:36 +0200

    add folder structure

--- main.py
+++
@@ -1,8 +0,0 @@
-print("What is your hometown?")
-first_part = input()
-
-print("What is your pet's name?")
-second_part = input()
-
-name = first_part + " " + second_part
-print(f"Your band name is: {name}.")
