cd3624245ae2869f646a3575a4c3e4d8cb7f0866
Kevin S. <kefin87@gmail.com>
2024-05-26 18:37:41 +0200

    more pythonic

--- Day1/main.py
+++ Day1/main.py
@@ -1,8 +1,5 @@
-print("What is your hometown?")
-first_part = input()
-
-print("What is your pet's name?")
-second_part = input()
+first_part = input("What is your hometown?\n")
+second_part = input("What is your pet's name?\n")
 
 name = first_part + " " + second_part
 print(f"Your band name is: {name}.")
